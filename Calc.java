575d5933e578220565d3f1d6e33972ff7182d889
Serg <serg@localhost.localdomain>
2016-06-10 06:22:05 -0400

    Added changes
    	modified:   Calc.java

--- Calc.java
+++ Calc.java
@@ -1,5 +1,9 @@
 public class Calc{
     public static void main(String[] args){
         System.out.println("Create repositiry on git");
+        int first = Integer.valueOf(args[0]);
+        int second = Integer.valueOf(args[1]);
+        int summ = first + second;
+        System.out.println("Summa = "+summ);
    }
 }
